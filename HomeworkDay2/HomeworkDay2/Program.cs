08012bf8596e2e591f4307c1a45645de4d8d31de
RobotOptimist <james.macivor1@gmail.com>
2014-11-11 07:13:28 -0500

    Day2 Homework
    Completed Day 2 homework

---
+++ HomeworkDay2/HomeworkDay2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkDay2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader myReader = new StreamReader("DecodeThis.txt");
+            StreamWriter myWriter = new StreamWriter("Decoded.txt");
+            string line = "";
+
+            while (line != null)
+            {
+                line = myReader.ReadLine();
+                if (line != null) 
+                {
+                    line = reverseString(line);
+                    Console.WriteLine(line);
+                    myWriter.WriteLine(line);
+                }
+            }
+                      
+            Console.ReadLine();
+            myReader.Dispose();
+            myWriter.Dispose();
+        }
+
+        private static string reverseString (string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray); 
+        }
+    
+    
+    }
+}
