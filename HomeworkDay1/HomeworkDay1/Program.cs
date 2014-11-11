f180261decdb7ea714e96725a6fc0fb44b5764c1
RobotOptimist <james.macivor1@gmail.com>
2014-11-11 06:25:44 -0500

    BasicC# Homework
    Created solution for learnvisualstudio.net day 1

---
+++ HomeworkDay1/HomeworkDay1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkDay1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Type in the sequence of numbers to prevent DOOM");
+            string userInput = Console.ReadLine();
+
+            if (userInput == "4 8 15 16 23 42")
+            {
+                Console.Beep();
+                Console.WriteLine("Success! Doom has been averted.");                
+            }
+            else
+            {
+                Console.Beep(1000, 1000);
+                Console.WriteLine("Failure!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
