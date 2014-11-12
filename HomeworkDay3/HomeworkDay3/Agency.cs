533def9d0b152632053ae4fc5158afe8e91ee8c1
RobotOptimist <james.macivor1@gmail.com>
2014-11-12 07:42:53 -0500

    Day3 Homework
    Translating a UML diagram to classes

---
+++ HomeworkDay3/HomeworkDay3/Agency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkDay3
+{
+    class Agency
+    {
+        public string AgencyName { get; set; }
+        public string Country { get; set; }
+        public int OrganizationSize { get; set; }
+
+        public string GetAgencyInfo() 
+        {
+            string info = "{0} -- {1} -- {2}", AgencyName, Country, OrganizationSize;
+            return info;
+        }
+
+ 
+    }
+}
---
+++ HomeworkDay3/HomeworkDay3/Spy.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkDay3
+{
+    class Spy
+    {
+        public string CodeName;
+        public Agency Agency;
+        public DateTime DateLastSeen;
+        public string Notes;
+
+        public void DisplaySpyInfo()
+        {
+            //TODO: Implement This           
+        }
+    }
+}
